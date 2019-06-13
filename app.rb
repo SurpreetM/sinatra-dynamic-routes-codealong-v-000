9d2ccf2aeba69a8777320835f6f4185ec4b8c0ce
surpreet.mundra <surpreet.mundra@gmail.com>
2019-06-13 14:28:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,4 +20,8 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1].to_i
+    @num2 = params [:num2].to_1
+    return @num1 * @num2
 end
